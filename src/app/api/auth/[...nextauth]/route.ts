c4b498f760eb04eadc8ec6ba5882260f571ac9a6
shi0n0 <shiono.yui1080@gmail.com>
2023-09-18 00:12:59 +0900

    GoogleProviderを追加

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google';
+
 
 const handler = NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
@@ -7,6 +9,10 @@ const handler = NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID ?? '',
       clientSecret: process.env.GITHUB_CLIENT_SECRET ?? '',
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
     }),
   ],
 })
