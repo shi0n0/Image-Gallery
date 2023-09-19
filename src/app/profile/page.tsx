d35ff0435648aa0d07343eee052a36a29eda8cff
shi0n0 <shiono.yui1080@gmail.com>
2023-09-19 16:35:09 +0900

    プロフィールページを仮追加

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function NotFound() {
+    return(
+        <>
+            <div className='text-xl'>プロフィール</div>
+            <Link className='underline hover:text-blue-500' href="/">ホームへ戻る</Link>
+        </>
+    )
+}
