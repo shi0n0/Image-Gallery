6bd3a4d089b8e84332ff6be12ed4f2badc3a716d
shi0n0 <shiono.yui1080@gmail.com>
2023-09-19 17:04:37 +0900

    ログイン関係の処理はプロフィールに集約

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,15 @@
 "use client"
 import Link from 'next/link'
-import { signOut } from 'next-auth/react'
+import { signIn, signOut } from 'next-auth/react'
 
 export default function NotFound() {
     return(
         <>
             <div className='text-xl'>プロフィール</div>
             <Link className='underline hover:text-blue-500' href="/">ホームへ戻る</Link>
+            <button onClick={() => signIn()} className="text-gray-600 hover:text-gray-800 px-4 py-2">
+              ログイン
+            </button>
             <button onClick={() => signOut()}>Sign out</button>
         </>
     )
