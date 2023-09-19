4c10f84a0a399742c63b9b02cae417688f393bb5
shi0n0 <shiono.yui1080@gmail.com>
2023-09-19 16:34:43 +0900

    ナビゲーションをコンポーネント化

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,56 @@
+"use client"
+
+import { signIn, signOut, useSession } from 'next-auth/react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Navbar() {
+  const { data: session } = useSession()
+
+  const log = () => {
+    console.log(session)
+  }
+
+  return (
+    <div className="bg-gray-100 font-sans">
+      {/* ナビゲーションバー */}
+      <nav className="bg-white shadow-lg">
+        <div className="container mx-auto px-1">
+          <div className="flex justify-between items-center py-4">
+            <div className="text-2xl font-bold">ImageGallery</div>
+            <div className="flex items-center space-x-4"> {/* ユーザーアイコンを含む flex コンテナ */}
+              <Link
+              href={"/"}
+              className='text-gray-600 hover:text-gray-800 px-4 py-2'
+              >
+              ホーム
+              </Link>
+              <a href="#" className="text-gray-600 hover:text-gray-800 px-4 py-2">投稿する</a>
+              <a href="#" className="text-gray-600 hover:text-gray-800 px-4 py-2">お気に入り</a>
+              {session && ( // セッションが存在する場合にユーザーアイコンを表示
+                <div className="relative">
+                  <Link
+                  href={"/profile"}
+                  >
+                    <Image
+                      src={session?.user?.image || 'https://kotonohaworks.com/free-icons/wp-content/uploads/kkrn_icon_user_1.png'}
+                      alt="ユーザーアイコン"
+                      width={40}
+                      height={40}
+                      className="rounded-full"
+                  />
+                  </Link>
+                </div>
+              )}
+              {!session && (
+                <button onClick={() => signIn()} className="text-gray-600 hover:text-gray-800 px-4 py-2">
+                  ログイン
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  )
+}
