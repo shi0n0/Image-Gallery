866640ce9f0f0eb50885e6b6d117164cc67780ad
shi0n0 <shiono.yui1080@gmail.com>
2023-09-21 15:29:07 +0900

    アイコンと名前を表示&セッションの有無でボタンを変化

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,16 +1,30 @@
 "use client"
 import Link from 'next/link'
-import { signIn, signOut } from 'next-auth/react'
+import Image from 'next/image'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
-export default function NotFound() {
+
+export default function Profile() {
+  const { data: session } = useSession()
+
     return(
         <>
-            <div className='text-xl'>プロフィール</div>
-            <Link className='underline hover:text-blue-500' href="/">ホームへ戻る</Link>
-            <button onClick={() => signIn()} className="text-gray-600 hover:text-gray-800 px-4 py-2">
-              ログイン
-            </button>
-            <button onClick={() => signOut()}>Sign out</button>
+            <div className='flex items-end'>
+              <Image
+                src={session?.user?.image || 'https://kotonohaworks.com/free-icons/wp-content/uploads/kkrn_icon_user_1.png'}
+                alt="ユーザーアイコン"
+                width={100}
+                height={100}
+                className="rounded-full"
+              />
+              <p className='text-5xl'>{session?.user?.name || "名無し"}</p>
+            </div>
+            {session && (
+              <button onClick={() => signOut()}>ログアウト</button>
+            )}
+            {!session && (
+              <button onClick={() => signIn()} className="text-gray-600 hover:text-gray-800 px-4 py-2">ログイン</button>
+            )} 
         </>
     )
 }
