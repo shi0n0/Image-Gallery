fcb21a9d0c52c4a9d24d81232a36a8189b618923
shi0n0 <shiono.yui1080@gmail.com>
2023-09-23 23:05:37 +0900

    簡易的な404ページに改変

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,9 +2,13 @@ import Link from 'next/link'
 
 export default function NotFound() {
     return(
-        <>
-            <div className='text-xl'>このページは存在しません</div>
-            <Link className='underline hover:text-blue-500' href="/">ホームへ戻る</Link>
-        </>
+        <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold mb-4">ページが見つかりません</h1>
+        <p className="text-gray-600 mb-8">申し訳ありませんが、お探しのページは存在しないようです。</p>
+        <img src="/your-custom-404-image.png" alt="404イメージ" className="max-w-sm" />
+        <Link href="/">
+          <p className="mt-8 text-blue-500 hover:underline">ホームに戻る</p>
+        </Link>
+      </div>
     )
 }
