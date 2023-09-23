11c168a9116f0f029de603de8a35e8bb7ca6e8a7
shi0n0 <shiono.yui1080@gmail.com>
2023-09-23 23:11:33 +0900

    ローディングをよりわかりやすいものに変更

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,10 @@
 export default function Loading() {
   return(
-      <div className="flex justify-center" aria-label="読み込み中">
-          <div className="animate-spin h-8 w-8 bg-blue-300 rounded-xl"></div>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="w-16 h-16 mb-4">
+        <div className="w-full h-full border-t-4 border-blue-500 rounded-full animate-spin"></div>
       </div>
+      <p className="text-lg font-semibold text-blue-500">ページを読み込んでいます...</p>
+    </div>
   )
 }
