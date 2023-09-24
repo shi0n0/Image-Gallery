7b6d05369b9d177bef795d0f1db00694ec5f6361
shi0n0 <shiono.yui1080@gmail.com>
2023-09-24 23:46:01 +0900

    日本語に翻訳

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -10,20 +10,18 @@ export default function Error({
   reset: () => void
 }) {
   useEffect(() => {
-    // Log the error to an error reporting service
     console.error(error)
   }, [error])
  
   return (
     <div>
-      <h2>Something went wrong!</h2>
+      <h2>何かが間違っています！</h2>
       <button
         onClick={
-          // Attempt to recover by trying to re-render the segment
           () => reset()
         }
       >
-        Try again
+        リトライ
       </button>
     </div>
   )
