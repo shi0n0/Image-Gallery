c1df6fd11b8760f059e4786652dffccb264fa100
shi0n0 <shiono.yui1080@gmail.com>
2023-09-26 23:56:35 +0900

    認証機能はホームに必要なくなったので一旦全て削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,4 @@
-"use client"
-
-import { signIn, signOut, useSession } from 'next-auth/react'
-import Image from 'next/image'
-import Link from 'next/link'
-
 export default function Home() {
-  const { data: session } = useSession()
-
 
   return (
     <main>
