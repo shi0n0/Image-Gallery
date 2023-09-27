dd413532c12702ff4ada49f796295f3232aa5f4d
shi0n0 <shiono.yui1080@gmail.com>
2023-09-27 17:39:08 +0900

    画像をsupabaseStorageにアップロードできるように

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,5 +1,26 @@
+"use client"
+import { ChangeEvent } from "react";
+import supabase from "../utils/supabase";
 
 export default function Profile() {
+
+  const handleUpload = async (e:ChangeEvent<HTMLInputElement>) => {
+    let file;
+
+    if (e.target.files){
+      file = e.target.files[0];
+    }
+
+    const { data, error } = await supabase.storage
+      .from("Images")
+      .upload("Public-" + file?.name, file as File)
+    
+    if (data) {
+      console.log(data)
+    } else if (error) {
+      console.log(error)
+    }
+  };
 
   return (
     <div className="max-w-lg mx-auto mt-8 p-4">
@@ -7,7 +28,12 @@ export default function Profile() {
       <div className="mb-4">
         <input
           type="file"
+          accept="image/*"
           className="border p-2"
+          id="file_input"
+          onChange={(e) => 
+            handleUpload(e)
+          }
         />
       </div>
       <div className="mb-4">
