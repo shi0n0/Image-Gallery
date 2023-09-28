b3399c62df4d18d1ce1dbbc1221f0a1710430579
shi0n0 <shiono.yui1080@gmail.com>
2023-09-29 02:31:58 +0900

    ボタン押下で情報をアップロードするように

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,33 +1,58 @@
 "use client"
-import { ChangeEvent } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import supabase from "../utils/supabase";
+import { log } from "console";
 
 export default function Profile() {
 
-  const handleUpload = async (e:ChangeEvent<HTMLInputElement>) => {
-    let file;
+const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
 
-    if (e.target.files){
-      file = e.target.files[0];
+  useEffect(() => {
+    // クライアントサイドでのみ実行する初期化コードをここに配置
+    setSelectedImage(new File([], ""));
+  }, []);
+  const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setSelectedImage(event.target.files[0]);
     }
-    const filePath = `Images/${file.name}`
+  };
 
-    const { error } = await supabase.storage
-      .from("Images")
-      .upload("Public-" + file?.name, file as File)
-    
-    if (error) {
-      console.log(error)
+  const handleSave = async () => {
+    if (!selectedImage) {
+      console.error("画像が選択されていません。");
+      return;
+    }
+
+    const filePath = `${selectedImage.name}`;
+
+    // 画像をストレージにアップロード
+    const { error: storageError } = await supabase.storage
+      .from('Images')
+      .upload(filePath, selectedImage);
+
+    if (storageError) {
+      console.error('ストレージエラー:', storageError.message);
+      return;
     }
 
     // 画像のURLを取得
-    const { data } = supabase.storage.from('Images').getPublicUrl(filePath)
-    const imageUrl = data.publicUrl
-    
-    // 画像のURLをDBに保存
+    const { data } = await supabase.storage.from('Images').getPublicUrl(filePath);
+    const imageUrl = data.publicUrl;
+
+    // 画像のURL、タイトル、および概要をDBに保存
     const { error: databaseError } = await supabase
-      .from('Images')
-      .insert({ imageUrl: imageUrl })
+      .from('Image')
+      .insert([
+        { url: imageUrl, title: title, description: description }
+      ]);
+
+    if (databaseError) {
+      console.error('データベースエラー:', databaseError.message);
+    } else {
+      console.log('データベースにデータを挿入しました。');
+    }
   };
 
   return (
@@ -39,9 +64,7 @@ export default function Profile() {
           accept="image/*"
           className="border p-2"
           id="file_input"
-          onChange={(e) => 
-            handleUpload(e)
-          }
+          onChange={handleImageChange}
         />
       </div>
       <div className="mb-4">
@@ -49,15 +72,18 @@ export default function Profile() {
           type="text"
           placeholder="タイトル"
           className="border p-2 w-full"
+          onChange={(e) => setTitle(e.target.value)}
         />
       </div>
       <div className="mb-4">
         <textarea
           placeholder="概要"
           className="border p-2 w-full h-24"
+          onChange={(e) => setDescription(e.target.value)}
         />
       </div>
       <button
+        onClick={handleSave}
         className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
       >
         送信
