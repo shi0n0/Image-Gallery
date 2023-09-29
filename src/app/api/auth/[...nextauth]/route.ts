9b59b919b94fa2e718a5e55442557297fe7583a8
shi0n0 <shiono.yui1080@gmail.com>
2023-09-30 00:25:53 +0900

    callbackでidを追加

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -19,6 +19,15 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
     }),
   ],
-})
+  callbacks: {
+    session: async ({ session, user }) => {
+      if (session?.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  }, 
+});
+
 
 export { handler as GET, handler as POST }
