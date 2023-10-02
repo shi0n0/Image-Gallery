a25e2754dcb35532609284f84f934a982da49944
shi0n0 <shiono.yui1080@gmail.com>
2023-10-02 23:57:59 +0900

    ホームに投稿した画像を表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,44 @@
+"use client"
+
+import { useEffect, useState } from 'react';
+import supabase from './utils/supabase';
+import Image from 'next/image';
+import { useSession } from "next-auth/react";
+
 export default function Home() {
+  const { data: session } = useSession()
+  const [userImages, setUserImages] = useState([]);
+  const userId = session?.user?.id
+  
+  useEffect(() => {
+    async function fetchUserImages() {
+      const { data, error } = await supabase
+        .from('Image')
+        .select('image_url')
+        .eq('user_id', userId); // ユーザーIDに基づいてフィルタリング
+
+      if (error) {
+        console.error('Error fetching user images:', error);
+      } else {
+        setUserImages(data);
+      }
+    }
+
+    fetchUserImages();
+  }, [userId]);
 
   return (
     <main>
-      
+      {userImages.map((image, index) => (
+        <div key={index}>
+          <Image
+            src={image.image_url}
+            alt={`User Image ${index}`}
+            width={500}
+            height={300}
+          />
+        </div>
+      ))}
     </main>
-  )
+  );
 }
