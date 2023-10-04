c9545e790d5453def4488f799c676b9e63ca16a4
shi0n0 <shiono.yui1080@gmail.com>
2023-10-04 14:12:21 +0900

    カードデザインの作成と遷移の追加

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -1,26 +1,29 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from 'react';
-import supabase from '../utils/supabase';
-import Image from 'next/image';
+import { useEffect, useState } from "react";
+import supabase from "../utils/supabase";
+import Image from "next/image";
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 
 export default function Card() {
-  const { data: session } = useSession()
-  const [userImages, setUserImages] = useState<{ url: string, title: string }[]>([]);
-  const userId = session?.user?.id
+  const { data: session } = useSession();
+  const [userImages, setUserImages] = useState<
+    { url: string; title: string }[]
+  >([]);
+  const userId = session?.user?.id;
 
   useEffect(() => {
     async function fetchUserImages() {
       if (userId) {
         // ユーザーIDが存在する場合のみクエリを実行
         const { data, error } = await supabase
-          .from('Image')
-          .select('url,title')
-          .eq('userId', userId);
+          .from("Image")
+          .select("url,title")
+          .eq("userId", userId);
 
         if (error) {
-          console.error('Error fetching user images:', error);
+          console.error("Error fetching user images:", error);
         } else {
           setUserImages(data);
         }
@@ -31,15 +34,25 @@ export default function Card() {
   }, [userId]);
 
   return (
-    <div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {userImages.map((image, index) => (
-        <div key={index} className='relative w-96 h-80'>
-          <Image
-            src={image.url}
-            alt={`User Image ${index}`}
-            fill
-          />
-          <p>{image.title}</p>
+        <div key={index} className="bg-white rounded-lg shadow-md">
+          <Link
+          href="profile" //詳細ページ未作成のため仮に/profileとする
+          >
+            <div className="relative h-0 pb-[75%]">
+              <Image
+                src={image.url}
+                alt={`User Image ${index}`}
+                layout="fill"
+                objectFit="cover"
+                className="rounded-t-lg"
+              />
+            </div>
+            <div className="p-4">
+              <p className="text-lg font-semibold">{image.title}</p>
+            </div>
+          </Link>
         </div>
       ))}
     </div>
