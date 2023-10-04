d29b673af993f3fd2656c9393056ab6fad21c780
shi0n0 <shiono.yui1080@gmail.com>
2023-10-04 23:58:37 +0900

    sessionから情報を取らずデータと一致する画像とユーザーを表示

---
+++ src/app/components/topUserCard.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import supabase from "../utils/supabase";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+
+export default function TopUserCard() {
+  const { data: session } = useSession();
+  const [userImages, setUserImages] = useState<
+    { url: string; title: string; userId: string }[]
+  >([]);
+  const [userProps, setUserProps] = useState<
+    { id: string; image: string; name: string }[]
+  >([]);
+  
+
+  useEffect(() => {
+    async function fetchUserImages() {
+        const { data:imagesData, error:imagesError } = await supabase
+          .from("Image")
+          .select("url,title,userId")
+
+        const { data:usersData, error:usersError} = await supabase
+          .from("User")
+          .select("id,image,name");
+
+        if (imagesError) {
+          console.error("Error fetching user images:", imagesError);
+        } else {
+          setUserImages(imagesData);
+        }
+
+        if (usersError) {
+          console.error("Error fetching data from users:", usersError)
+        } else {
+          setUserProps(usersData);
+        }
+        
+
+    }
+
+    fetchUserImages();
+  }, []);
+
+  
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      {userImages.map((image, index) => {
+        const matchingUser = userProps.find((user) => user.id === image.userId);
+
+        return (
+          <div key={index} className="bg-white shadow-md">
+            <Link href="/profile">
+              <div className="relative rounded-none md:h-0 pb-[75%] overflow-hidden rounded-t-lg">
+                <Image
+                  src={image.url}
+                  alt={`User Image ${index}`}
+                  layout="fill"
+                  objectFit="cover"
+                  className="transition duration-300 hover:scale-110"
+                />
+              </div>
+              <div className="relative p-4">
+                <p className="text-3xl font-semibold">{image.title}</p>
+                <div className="flex justify-end items-center">
+                  <Image
+                    src={
+                      matchingUser?.image ||
+                      "https://kotonohaworks.com/free-icons/wp-content/uploads/kkrn_icon_user_1.png"
+                    }
+                    alt="User Icon"
+                    objectFit="cover"
+                    className="rounded-none md:rounded-full mr-1"
+                    width={40}
+                    height={40}
+                  />
+                  <p className="text-2xl">{matchingUser?.name || "Unknown"}</p>
+                </div>
+              </div>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
