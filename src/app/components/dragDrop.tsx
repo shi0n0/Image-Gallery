c3d6a3da047e47e30c5a9366323910d864c75a60
shi0n0 <shiono.yui1080@gmail.com>
2023-10-06 02:17:12 +0900

    ヘッダーのアップロードと表示

--- src/app/components/dragDrop.tsx
+++ src/app/components/dragDrop.tsx
@@ -1,19 +1,99 @@
-import React, { useState } from "react";
+"use client";
+
+import React, { ChangeEvent, useEffect, useState } from "react";
 import Image from "next/image";
 import Auth from "./auth";
+import { useSession } from "next-auth/react";
+import supabase from "../utils/supabase";
+import router from "next/router";
 
 const fileTypes = ["JPG", "PNG", "GIF"];
 
 function DragDrop() {
-  const [file, setFile] = useState(null);
-  const handleChange = (file: React.SetStateAction<null>) => {
-    setFile(file);
+  const { data: session } = useSession();
+  const [header, setHeader] = useState<File | null>(null);
+  const [userHeaders, setUserHeaders] = useState<
+    { url: string }[]
+  >([]);
+  const userId = session?.user?.id;
+
+  useEffect(() => {
+    // クライアントサイドでのみ実行する初期化コードをここに配置
+    setHeader(new File([], ""));
+  }, []);
+  const handleHeaderChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setHeader(event.target.files[0]);
+    }
   };
 
+  const handleSave = async () => {
+    if (!header) {
+      console.error("ヘッダー画像が選択されていません！");
+      return;
+    }
+
+    const filePath = `${header.name}`;
+
+    // 画像をストレージにアップロード
+    const { error: storageError } = await supabase.storage
+      .from("Headers")
+      .upload(filePath, header);
+
+    if (storageError) {
+      console.error("ストレージエラー:", storageError.message);
+      return;
+    }
+
+    // 画像のURLを取得
+    const { data } = await supabase.storage
+      .from("Headers")
+      .getPublicUrl(filePath);
+    const imageUrl = data.publicUrl;
+
+    const userId = session?.user?.id;
+
+    // 画像のURLをDBに保存
+    const { error: databaseError } = await supabase
+      .from("Header")
+      .insert([
+        {
+          url: imageUrl,
+          userId: userId,
+        },
+      ]);
+
+    if (databaseError) {
+      console.error("データベースエラー:", databaseError.message);
+    } else {
+      console.log("データベースにデータを挿入しました。");    }
+  };
+
+  useEffect(() => {
+    async function fetchUserHeaders() {
+      if (userId) {
+        // ユーザーIDが存在する場合のみクエリを実行
+        const { data, error } = await supabase
+          .from("Header")
+          .select("url")
+          .eq("userId", userId);
+
+        if (error) {
+          console.error("Error fetching user images:", error);
+        } else {
+          setUserHeaders(data);
+          console.log(userHeaders)
+        }
+      }
+    }
+
+    fetchUserHeaders();
+  }, [userId]);
+
   return (
-    <div className="relative w-full h-60 flex justify-center z-10">
+    <div className="relative w-full h-60 flex justify-center z-0">
       <Image
-        src="/ImageGallery-30.png"
+        src={userHeaders.length > 0 ? userHeaders[0].url : "/ImageGallery-30.png"}
         alt="デフォルトヘッダー"
         objectFit="cover"
         className="opacity-50"
@@ -23,16 +103,18 @@ function DragDrop() {
       <div className="absolute bottom-5 right-5 z-20">
         <label
           htmlFor="fileInput"
-          className="px-4 py-3.5 bg-gray-300 rounded-full cursor-pointer opacity-50 hover:opacity-100"
+          className="px-4 py-3.5 text-white bg-black rounded-full cursor-pointer opacity-10 transition-opacity duration-150 hover:opacity-50"
+          onChange={handleSave}
         >
           ＋
-        <input
-          id="fileInput"
-          type="file"
-          accept="image/*"
-          className="hidden"
-        />
-      </label>
+          <input
+            id="fileInput"
+            type="file"
+            accept="image/*"
+            className="hidden"
+            onChange={handleHeaderChange}
+          />
+        </label>
       </div>
     </div>
   );
