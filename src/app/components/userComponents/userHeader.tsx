64568bbd46f05f3ad10e473cd2ea017f73e74311
shi0n0 <shiono.yui1080@gmail.com>
2023-10-10 00:29:11 +0900

    URLからuserIdを取得し一致するヘッダーを表示

--- src/app/components/userComponents/userHeader.tsx
+++ src/app/components/userComponents/userHeader.tsx
@@ -1,6 +1,45 @@
-export default function UserHeader() {
+"use client";
 
-  return(
-    <div>ユーザーヘッダー</div>
-  )
+import supabase from "@/app/utils/supabase";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
+export default function UserHeader() {
+  const [userHeader, setUserHeader] = useState("");
+  const getPagePath = usePathname();
+  const userId = getPagePath.replace("/userprofile/", "");
+
+  useEffect(() => {
+    async function fetchuserHeader() {
+      const { data, error } = await supabase // URLのユーザーIDをもとにヘッダーを取得
+        .from("Header")
+        .select("url")
+        .eq("userId", userId);
+
+      if (error) {
+        console.error(error);
+      } else {
+        const headerImageUrl =
+          data.length > 0 ? data[0].url : "/ImageGallery-30.png";
+        setUserHeader(headerImageUrl);
+      }
+    }
+    fetchuserHeader();
+  }, [userId]);
+
+  console.log(userHeader);
+
+  return (
+    <div>
+      <div>ユーザーヘッダー</div>
+      <Image
+        src={userHeader || "/ImageGallery-30.png"}
+        alt="デフォルトヘッダー"
+        objectFit="cover"
+        className="opacity-50"
+        fill
+      />
+    </div>
+  );
 }
