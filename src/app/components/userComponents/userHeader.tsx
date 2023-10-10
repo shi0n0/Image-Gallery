335b8eee4d3b6ab5347172ec99f358511738d00d
shi0n0 <shiono.yui1080@gmail.com>
2023-10-10 14:18:56 +0900

    ユーザープロフィールページの作成

--- src/app/components/userComponents/userHeader.tsx
+++ src/app/components/userComponents/userHeader.tsx
@@ -4,42 +4,67 @@ import supabase from "@/app/utils/supabase";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import Auth from "../myComponents/auth";
 
 export default function UserHeader() {
   const [userHeader, setUserHeader] = useState("");
+  const [userProps, setUserProps] = useState<{ id: string; image: string; name: string }[]>([]);
   const getPagePath = usePathname();
   const userId = getPagePath.replace("/userprofile/", "");
 
   useEffect(() => {
     async function fetchuserHeader() {
-      const { data, error } = await supabase // URLのユーザーIDをもとにヘッダーを取得
+      const { data: headerData, error: headerError } = await supabase // ヘッダー画像の取得
         .from("Header")
         .select("url")
         .eq("userId", userId);
-
-      if (error) {
-        console.error(error);
+  
+      const { data: userData, error: userError } = await supabase
+        .from("User")
+        .select("id, image, name")
+        .eq("id", userId);
+  
+      if (headerError) {
+        console.error("ヘッダー画像の取得エラー:", headerError);
       } else {
         const headerImageUrl =
-          data.length > 0 ? data[0].url : "/ImageGallery-30.png";
+          headerData.length > 0 ? headerData[0].url : "/ImageGallery-30.png";
         setUserHeader(headerImageUrl);
+      }
+  
+      if (userError) {
+        console.error("ユーザー情報の取得エラー:", userError);
+      } else {
+        setUserProps(userData);
       }
     }
     fetchuserHeader();
   }, [userId]);
-
-  console.log(userHeader);
-
+  
+  
   return (
     <div>
-      <div>ユーザーヘッダー</div>
-      <Image
-        src={userHeader || "/ImageGallery-30.png"}
-        alt="デフォルトヘッダー"
-        objectFit="cover"
-        className="opacity-50"
-        fill
-      />
+      <div className="relative w-full h-60 flex justify-center z-0">
+        <Image
+          src={userHeader || "/ImageGallery-30.png"}
+          alt="デフォルトヘッダー"
+          objectFit="cover"
+          className="opacity-50"
+          fill
+        />
+        <div className="z-10 p-3 flex items-center flex-col">
+          <div className="relative w-[100px] h-[100px]">
+            <Image
+              src={userProps.length > 0 ? userProps[0].image : "/default-image.png"}
+              alt="ユーザーアイコン"
+              objectFit="cover"
+              className="rounded-full"
+              fill
+            />
+          </div>
+          <p className="text-5xl font-midium">{userProps.length > 0 ? userProps[0].name : "名無し"}</p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/components/userComponents/userProfileCard.tsx
+++ src/app/components/userComponents/userProfileCard.tsx
@@ -1,6 +1,93 @@
-export default function UserProfileCard() {
+"use client"
 
-  return(
-    <div>ユーザープロフィールカード</div>
-  )
+import supabase from "@/app/utils/supabase";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function UserProfileCard() {
+  const getPagePath = usePathname();
+  const userId = getPagePath.replace("/userprofile/", "");
+  const [userImages, setUserImages] = useState<
+    { id: string; url: string; title: string }[]
+  >([]);
+  const [userAccounts, setUserAccounts] = useState<
+    { id: string; image: string; name: string }[]
+  >([]);
+
+  useEffect(() => {
+    async function fetchUserData() {
+      if (userId) {
+        // ユーザー画像データの取得
+        const { data: userImageData, error: userImageError } = await supabase
+          .from("Image")
+          .select("id,url,title")
+          .eq("userId", userId);
+
+        // ユーザーアカウントデータの取得
+        const { data: userAccountData, error: userAccountError } = await supabase
+          .from("User")
+          .select("id,image,name")
+          .eq("id", userId);
+
+        if (userImageError) {
+          console.error("ユーザー画像の取得エラー:", userImageError);
+        } else {
+          setUserImages(userImageData);
+        }
+
+        if (userAccountError) {
+          console.error("ユーザーアカウントの取得エラー", userAccountError);
+        } else {
+          setUserAccounts(userAccountData);
+        }
+      }
+    }
+
+    fetchUserData();
+  }, [userId]);
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 p-10">
+      {userImages.map((image, index) => {
+
+      const userAccount = userAccounts.find((account) => account.id === userId);
+
+        return (
+          <div
+            key={index}
+            className="bg-white shadow-md rounded-lg overflow-hidden"
+          >
+            <Link href={`/illustrations/${image.id}`}>
+              <div className="relative pb-[75%]">
+                <Image
+                  src={image.url}
+                  alt={`User Image ${index}`}
+                  layout="fill"
+                  objectFit="cover"
+                  quality={50}
+                  className="transition duration-300 hover:scale-110"
+                />
+              </div>
+              <div className="p-4">
+                <p className="text-2xl font-semibold">{image.title}</p>
+                <div className="flex justify-end items-center mt-2">
+                  <Image
+                    src={userAccount?.image || "デフォルトのアイコンURL"}
+                    alt="User Icon"
+                    objectFit="cover"
+                    className="rounded-full mr-2"
+                    width={40}
+                    height={40}
+                  />
+                  <p className="text-xl">{userAccount?.name || "Unknown"}</p>
+                </div>
+              </div>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
