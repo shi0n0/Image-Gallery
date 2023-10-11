bfa10af4f18f0f1174c8c339e0698c1868bfceb2
shi0n0 <shiono.yui1080@gmail.com>
2023-10-11 17:30:27 +0900

    バックグラウンドの色を変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import NextAuthProvider from '@/providers/NextAuth'
 import Navbar from './components/commonComponents/navbar'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Image-Gallery',
@@ -14,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='ja'>
-      <body className={inter.className}>
+      <body className="bg-gray-50">
         <NextAuthProvider>
           <Navbar />
           <main>
