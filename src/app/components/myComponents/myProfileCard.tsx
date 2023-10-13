2cb36874c5c43b9434c2ac09a0f34c5a2a1da28d
shi0n0 <shiono.yui1080@gmail.com>
2023-10-13 16:56:08 +0900

    コードを修正し可読性を向上させた

--- src/app/components/myComponents/myProfileCard.tsx
+++ src/app/components/myComponents/myProfileCard.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useEffect, useState } from "react";
 import supabase from "../../utils/supabase";
 import Image from "next/image";
@@ -8,25 +6,25 @@ import Link from "next/link";
 
 export default function ProfileCard() {
   const { data: session } = useSession();
+  const userId = session?.user?.id;
+
   const [userImages, setUserImages] = useState<
     { id: string; url: string; title: string; userId: string }[]
   >([]);
-  const userId = session?.user?.id;
 
   useEffect(() => {
     async function fetchUserImages() {
-      if (userId) {
-        // ユーザーIDが存在する場合のみクエリを実行
-        const { data, error } = await supabase
-          .from("Image")
-          .select("id,url, title, userId")
-          .eq("userId", userId);
+      if (!userId) return;
 
-        if (error) {
-          console.error("ユーザー画像の取得エラー:", error);
-        } else {
-          setUserImages(data);
-        }
+      const { data, error } = await supabase
+        .from("Image")
+        .select("id, url, title, userId")
+        .eq("userId", userId);
+
+      if (error) {
+        console.error("ユーザー画像の取得エラー:", error);
+      } else {
+        setUserImages(data);
       }
     }
 
@@ -34,43 +32,39 @@ export default function ProfileCard() {
   }, [userId]);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 p-10">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 p-6">
       {userImages.map((image, index) => (
         <div
           key={index}
-          className="bg-white shadow-md rounded-lg overflow-hidden"
+          className="bg-white rounded-lg overflow-hidden shadow-sm hover:shadow-md transition duration-300 hover:scale-105"
         >
           <Link href={`illustrations/${image.id}`}>
-            <div className="relative pb-[75%]">
+            <div className="relative pb-[65%]">
               <Image
                 src={image.url}
                 alt={`User Image ${index}`}
                 layout="fill"
                 objectFit="cover"
-                quality={50}
-                className="transition duration-300 hover:scale-110"
+                quality={75}
               />
             </div>
-            <div className="p-4">
-              <p className="text-2xl font-semibold">{image.title}</p>
-              <Link href={`/userprofile/${image.userId}`}>
-                <div className="flex justify-end items-center mt-2">
-                  <Image
-                    src={
-                      session?.user?.image ||
-                      "https://kotonohaworks.com/free-icons/wp-content/uploads/kkrn_icon_user_1.png"
-                    }
-                    alt="User Icon"
-                    objectFit="cover"
-                    className="rounded-full mr-2"
-                    width={40}
-                    height={40}
-                  />
-                  <p className="text-xl">{session?.user?.name}</p>
-                </div>
-              </Link>
-            </div>
           </Link>
+          <div className="p-4">
+            <p className="text-xl font-semibold">{image.title}</p>
+            <Link href={`/userprofile/${image.userId}`}>
+              <div className="flex items-center mt-2">
+                <Image
+                  src={session?.user?.image || "https://kotonohaworks.com/free-icons/wp-content/uploads/kkrn_icon_user_1.png"}
+                  alt="User Icon"
+                  objectFit="cover"
+                  className="rounded-full mr-2"
+                  width={40}
+                  height={40}
+                />
+                <p className="text-lg">{session?.user?.name}</p>
+              </div>
+            </Link>
+          </div>
         </div>
       ))}
     </div>
