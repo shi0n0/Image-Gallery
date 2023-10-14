2c89c93f999548d00e6d52bc9ed6f03441cd76ee
shi0n0 <shiono.yui1080@gmail.com>
2023-10-14 11:47:03 +0900

    投稿時間,ジャンルなどを仮設置,デザインの修正

--- src/app/components/commonComponents/topPageCard.tsx
+++ src/app/components/commonComponents/topPageCard.tsx
@@ -40,26 +40,38 @@ export default function TopUserCard() {
   }, []);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 p-10">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-10">
       {userImages.map((image, index) => {
         const matchingUser = userProps.find((user) => user.id === image.userId);
 
         return (
-          <div key={index} className="bg-white shadow-md rounded-lg overflow-hidden transition-transform transform hover:scale-105 hover:shadow-lg">
+          <div
+            key={index}
+            className="bg-white rounded-lg shadow-md p-4 duration-150 hover:-translate-y-1.5 "
+          >
             <Link href={`illustrations/${image.id}`}>
-              <div className="relative pb-[75%]">
+              <div className="relative w-full h-48">
                 <Image
                   src={image.url}
                   alt={`ユーザー画像 ${index}`}
-                  layout="fill"
                   objectFit="cover"
-                  quality={50}
+                  quality={10}
+                  className="w-full h-full rounded-lg"
+                  fill
                 />
+                <div className="absolute top-2 right-2">
+                  <span className="bg-red-500 text-white py-1 px-2 rounded-full text-xs">
+                    New
+                  </span>
+                </div>
               </div>
-              <div className="p-4">
-                <p className="text-2xl font-semibold">{image.title}</p>
+
+              <div className="mt-4">
+                <h2 className="text-xl font-semibold text-gray-800">
+                  {image.title}
+                </h2>
                 <Link href={`/userprofile/${matchingUser?.id}`}>
-                  <div className="flex justify-end items-center mt-2">
+                  <div className="flex items-center mt-2">
                     <Image
                       src={
                         matchingUser?.image ||
@@ -67,13 +79,48 @@ export default function TopUserCard() {
                       }
                       alt="ユーザーアイコン"
                       objectFit="cover"
-                      className="rounded-full mr-2"
+                      className="w-8 h-8 rounded-full"
                       width={40}
                       height={40}
                     />
-                    <p className="text-xl text-gray-700 duration-100 hover:text-gray-900">
+                    <p className="text-gray-600 ml-2">
                       {matchingUser?.name || "Unknown"}
                     </p>
+                  </div>
+
+                  <div className="flex items-center text-gray-600 text-sm mt-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-4 w-4 mr-1"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                      />
+                    </svg>
+                    <span>2023/10/14</span>
+                  </div>
+                  <div className="flex items-center text-gray-600 text-sm mt-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-4 w-4 mr-1"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M4 6h16M4 10h16M4 14h16M4 18h16"
+                      />
+                    </svg>
+                    <span>ねこ / 自然 / 動物</span>
                   </div>
                 </Link>
               </div>
