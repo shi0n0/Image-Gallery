ffe9c2e23a3617379f8fd102edb15708ab239536
shi0n0 <shiono.yui1080@gmail.com>
2023-10-17 17:55:07 +0900

    タイトルは機能外のため除外

--- src/app/components/myComponents/tabSwitch.tsx
+++ src/app/components/myComponents/tabSwitch.tsx
@@ -3,9 +3,6 @@ import Link from "next/link";
 export default function TabSwitch() {
   return (
     <>
-      <p className="text-2xl mb-5 font-semibold text-gray-700">
-        ダッシュボード
-      </p>
       <Link href="/dashboard">TOP</Link>
       <Link href="/dashboard/works">WORKS</Link>
     </>
