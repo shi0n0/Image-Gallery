e3059187f08f5edcb902c2b475d3e1557eca5948
shi0n0 <shiono.yui1080@gmail.com>
2023-10-18 22:18:41 +0900

    URLが一致していた場合のスタイルを追加

--- src/app/components/myComponents/tabSwitch.tsx
+++ src/app/components/myComponents/tabSwitch.tsx
@@ -1,10 +1,31 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function TabSwitch() {
+  const getPagePath = usePathname();
+  const pagePath = getPagePath.replace("/illustrations/", "");
+  console.log(pagePath);
+
   return (
     <div className="w-full mb-5">
-      <Link className="rounded-full py-2 px-4 m-2 duration-150 hover:bg-gray-200" href="/dashboard">TOP</Link>
-      <Link className="rounded-full py-2 px-4 m-2 duration-150 hover:bg-gray-200" href="/dashboard/works">WORKS</Link>
+      <Link
+        className={`rounded-full py-2 px-4 m-2 duration-200 text-gray-500 hover:text-gray-800 ${
+          pagePath == "/dashboard" ? "!text-gray-800 bg-gray-200" : ""
+        }`}
+        href="/dashboard"
+      >
+        TOP
+      </Link>
+      <Link
+        className={`rounded-full py-2 px-4 m-2 duration-200 text-gray-500 hover:text-gray-800 ${
+          pagePath == "/dashboard/works" ? "!text-gray-800 bg-gray-200" : ""
+        }`}
+        href="/dashboard/works"
+      >
+        WORKS
+      </Link>
     </div>
   );
 }
