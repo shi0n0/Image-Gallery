120a231caddde2274ed296d467d6912418c43bc6
shi0n0 <shiono.yui1080@gmail.com>
2023-10-18 18:11:39 +0900

    スタイルを追加

--- src/app/components/myComponents/tabSwitch.tsx
+++ src/app/components/myComponents/tabSwitch.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function TabSwitch() {
   return (
-    <>
-      <Link href="/dashboard">TOP</Link>
-      <Link href="/dashboard/works">WORKS</Link>
-    </>
+    <div className="w-full mb-5">
+      <Link className="rounded-full py-2 px-4 m-2 duration-150 hover:bg-gray-200" href="/dashboard">TOP</Link>
+      <Link className="rounded-full py-2 px-4 m-2 duration-150 hover:bg-gray-200" href="/dashboard/works">WORKS</Link>
+    </div>
   );
 }
