64d131169b6c263733378c7d99b0ae69ec32896e
shi0n0 <shiono.yui1080@gmail.com>
2023-10-20 17:37:35 +0900

    アップロードした画像の管理をするページ

---
+++ src/app/dashboard/edit/page.tsx
@@ -0,0 +1,19 @@
+import MyHeader from "@/app/components/myComponents/myHeader";
+import PaddingContainer from "@/app/components/commonComponents/paddingCotainer";
+import TabSwitch from "@/app/components/myComponents/tabSwitch";
+import EditCard from "@/app/components/myComponents/editCard";
+
+export default function Edit() {
+  return (
+    <div>
+      <MyHeader />
+      <PaddingContainer>
+        <p className="text-2xl mb-5 font-semibold text-gray-700">
+          ダッシュボード
+        </p>
+        <TabSwitch />
+        <EditCard />
+      </PaddingContainer>
+    </div>
+  );
+}
