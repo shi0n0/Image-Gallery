a73a36c92b6684323b52d7968d10eef77a165511
shi0n0 <shiono.yui1080@gmail.com>
2023-10-22 23:26:06 +0900

    編集ページに簡単なデザインを追加

--- src/app/components/myComponents/editCard.tsx
+++ src/app/components/myComponents/editCard.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
 import React, { useState, useEffect } from "react";
 import supabase from "@/app/utils/supabase";
 import { useSession } from "next-auth/react";
-import { useRouter,usePathname } from 'next/navigation'
+import { useRouter, usePathname } from "next/navigation";
 
 const EditImagePage: React.FC = () => {
   const { data: session } = useSession();
@@ -17,10 +17,13 @@ const EditImagePage: React.FC = () => {
   });
 
   useEffect(() => {
-
     const fetchImageData = async () => {
-      const { data, error } = await supabase.from("Image").select("*").eq("id", imageId).single();
-  
+      const { data, error } = await supabase
+        .from("Image")
+        .select("*")
+        .eq("id", imageId)
+        .single();
+
       if (error) {
         console.error("フェッチングに失敗:", error);
       } else {
@@ -31,16 +34,17 @@ const EditImagePage: React.FC = () => {
     fetchImageData();
   }, [imageId]);
 
-
-
   const handleSave = async () => {
     const { title, description } = imageData;
 
     // Update image data in Supabase
-    const { error } = await supabase.from("Image").update({
-      title,
-      description,
-    }).eq("id", imageId);
+    const { error } = await supabase
+      .from("Image")
+      .update({
+        title,
+        description,
+      })
+      .eq("id", imageId);
 
     if (error) {
       console.error("アップデートに失敗:", error);
@@ -56,32 +60,48 @@ const EditImagePage: React.FC = () => {
     if (error) {
       console.error("削除に失敗:", error);
     } else {
-      console.log("削除に失敗");
+      console.log("削除に成功");
       // Redirect to a different page after deletion
       router.push("/dashboard/edit");
     }
   };
 
   return (
-    <div>
-      <h1>Edit Image Page</h1>
-      <div>
-        <label>Title:</label>
+    <div className="p-4">
+      <h1 className="text-2xl mb-4">Edit Image Page</h1>
+      <div className="mb-4">
+        <label className="block">Title:</label>
         <input
           type="text"
           value={imageData.title}
-          onChange={(e) => setImageData({ ...imageData, title: e.target.value })}
+          onChange={(e) =>
+            setImageData({ ...imageData, title: e.target.value })
+          }
+          className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
         />
       </div>
-      <div>
-        <label>Description:</label>
+      <div className="mb-4">
+        <label className="block">Description:</label>
         <textarea
           value={imageData.description}
-          onChange={(e) => setImageData({ ...imageData, description: e.target.value })}
+          onChange={(e) =>
+            setImageData({ ...imageData, description: e.target.value })
+          }
+          className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
         />
       </div>
-      <button onClick={handleSave}>Save</button>
-      <button onClick={handleDelete}>Delete</button>
+      <button
+        onClick={handleSave}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-6 rounded-lg mr-2 transition duration-300 ease-in-out transform hover:scale-105"
+      >
+        Save
+      </button>
+      <button
+        onClick={handleDelete}
+        className="bg-red-500 hover:bg-red-700 text-white font-semibold py-2 px-6 rounded-lg transition duration-300 ease-in-out transform hover:scale-105"
+      >
+        Delete
+      </button>
     </div>
   );
 };
