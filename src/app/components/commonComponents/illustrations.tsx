124d018f762f054cfbaf260ab1d02d8d8a170321
shi0n0 <shiono.yui1080@gmail.com>
2023-10-27 23:32:17 +0900

    いいね,お気に入りを追加

--- src/app/components/commonComponents/illustrations.tsx
+++ src/app/components/commonComponents/illustrations.tsx
@@ -77,14 +77,14 @@ const ImageDetail = () => {
   const year = originalDateTime.getFullYear(); // 年
   const month = (originalDateTime.getMonth() + 1).toString().padStart(2, "0"); // 月
   const day = originalDateTime.getDate().toString().padStart(2, "0"); // 日
-  const hours = originalDateTime.getHours().toString().padStart(2, "0") // 時
-  const minutes = originalDateTime.getMinutes().toString().padStart(2, "0") //　分
+  const hours = originalDateTime.getHours().toString().padStart(2, "0"); // 時
+  const minutes = originalDateTime.getMinutes().toString().padStart(2, "0"); //　分
 
   const formattedDate = `${year}年${month}月${day}日 ${hours}:${minutes}`;
 
   return (
     <div className="p-10 flex">
-      <div className="w-4/6 pr-5">
+      <div className="w-5/6 pr-5">
         <div className="relative h-[60vh] bg-gray-200 rounded-lg overflow-hidden">
           <Image
             src={image.url || "/ImageGallery-30.png"}
@@ -95,38 +95,54 @@ const ImageDetail = () => {
           />
         </div>
       </div>
-
-      <div className="w-2/6 bg-white rounded-lg">
-        <div className="p-5">
-          <p className="text-3xl font-bold mb-1">
-            {image.title || "ダミータイトル"}
-          </p>
-          <p className="text-xl text-gray-800 mb-6">
-            {image.description || "ディスクリプションの文章がここに入ります"}
-          </p>
-          <p className="text-lg text-gray-500 mb-6">
-            {formattedDate || "◯◯◯◯年◯◯月◯◯日 "}
-          </p>
-
-          {user && (
-            <div className="flex items-center space-x-4">
-              <Link
-                href={`/userprofile/${user.id}`}
-                className="flex items-center p-2 hover:bg-gray-800 hover:bg-opacity-10 hover:rounded-lg"
-              >
-                <div className="w-16 h-16 relative rounded-full overflow-hidden">
-                  <Image
-                    src={user.image}
-                    alt="ユーザーのアイコン"
-                    className="object-cover w-full h-full"
-                    layout="fill"
-                  />
-                </div>
-                <p className="text-lg font-semibold px-2">{user.name}</p>
-              </Link>
-            </div>
-          )}
+      <div>
+        <div className="bg-white rounded-lg mb-5">
+          <div className="p-5">
+            <p className="text-3xl font-bold mb-1">
+              {image.title || "ダミータイトル"}
+            </p>
+            <p className="text-xl text-gray-800 mb-6">
+              {image.description || "ディスクリプションの文章がここに入ります"}
+            </p>
+            <p className="text-lg text-gray-500">
+              {formattedDate || "◯◯◯◯年◯◯月◯◯日 "}
+            </p>
+          </div>
         </div>
+
+        <div className="flex items-center space-x-1">
+          <div className="text-center">
+            <p className="text-sm text-gray-500 mb-1">いいね</p>
+            <span className="text-5xl text-red-500 bg-white rounded-lg cursor-pointer">
+              ♡
+            </span>
+          </div>
+          <div className="text-center">
+            <p className="text-sm text-gray-500 mb-1">お気に入り</p>
+            <span className="text-5xl text-yellow-300 bg-white rounded-lg cursor-pointer">
+              ☆
+            </span>
+          </div>
+        </div>
+
+        {user && (
+          <div className="flex items-center space-x-4">
+            <Link
+              href={`/userprofile/${user.id}`}
+              className="flex items-center p-2 hover:bg-gray-800 hover:bg-opacity-10 hover:rounded-lg"
+            >
+              <div className="w-16 h-16 relative rounded-full overflow-hidden">
+                <Image
+                  src={user.image}
+                  alt="ユーザーのアイコン"
+                  className="object-cover w-full h-full"
+                  layout="fill"
+                />
+              </div>
+              <p className="text-lg font-semibold px-2">{user.name}</p>
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
