18db04d84756c96989bec1952077d94765687441
shi0n0 <shiono.yui1080@gmail.com>
2023-10-28 15:10:53 +0900

    デザインを変更

--- src/app/components/myComponents/tabSwitch.tsx
+++ src/app/components/myComponents/tabSwitch.tsx
@@ -8,32 +8,48 @@ export default function TabSwitch() {
   const pagePath = getPagePath.replace("/illustrations/", "");
 
   return (
-    <div className="w-full mb-5">
+    <div className="w-full mb-5 flex">
       <Link
-        className={`rounded-full py-2 px-4 m-2 duration-200 text-gray-500 hover:text-gray-800 ${
-          pagePath == "/dashboard" ? "!text-gray-800 bg-gray-200" : ""
+        className={`py-2 px-4 m-2 duration-200 text-gray-500 hover:text-gray-800 ${
+          pagePath == "/dashboard"
+            ? "!text-gray-800 border-b-4 border-custom-pink "
+            : ""
         }`}
         href="/dashboard"
       >
-        ホーム
+        <p className={`${
+          pagePath == "/dashboard"
+            ? "transform -translate-y-1"
+            : ""
+        }`}>ホーム</p>
       </Link>
       <Link
-        className={`rounded-full py-2 px-4 m-2 duration-200 text-gray-500 hover:text-gray-800 ${
-          pagePath == "/dashboard/works" ? "!text-gray-800 bg-gray-200" : ""
+        className={`py-2 px-4 m-2 duration-200 text-gray-500 hover:text-gray-800 ${
+          pagePath == "/dashboard/works"
+            ? "!text-gray-800 border-b-4 border-custom-pink "
+            : ""
         }`}
         href="/dashboard/works"
       >
-        作品
+        <p className={`${
+          pagePath == "/dashboard/works"
+            ? "transform -translate-y-1"
+            : ""
+        }`}>作品</p>
       </Link>
       <Link
-        className={`rounded-full py-2 px-4 m-2 duration-200 text-gray-500 hover:text-gray-800 ${
+        className={`py-2 px-4 m-2 duration-200 text-gray-500 hover:text-gray-800 ${
           pagePath == "/dashboard/edit"
-            ? "!text-gray-800 bg-gray-200"
+            ? "!text-gray-800 border-b-4 border-custom-pink "
             : ""
         }`}
         href="/dashboard/edit"
       >
-        管理
+        <p className={`${
+          pagePath == "/dashboard/edit"
+            ? "transform -translate-y-1"
+            : ""
+        }`}>管理</p>
       </Link>
     </div>
   );
