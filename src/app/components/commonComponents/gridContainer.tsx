4a93bddbe780a7992213a5ae7f657bc1c87a32f3
shi0n0 <shiono.yui1080@gmail.com>
2023-10-29 19:49:50 +0900

    デザインを微調整

--- src/app/components/commonComponents/gridContainer.tsx
+++ src/app/components/commonComponents/gridContainer.tsx
@@ -7,7 +7,7 @@ interface PaddingContainerProps {
 const GridContainer: React.FC<PaddingContainerProps> = ({ children }) => {
 
   return(
-    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 px-10">
+    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2 px-10">
       {children}
     </div>
   )
