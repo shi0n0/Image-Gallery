14ab62327055df69a93c1f5fc69578f6a946e7aa
shi0n0 <shiono.yui1080@gmail.com>
2023-10-30 01:45:26 +0900

    オーバーレイを追加

--- src/app/components/commonComponents/navbar.tsx
+++ src/app/components/commonComponents/navbar.tsx
@@ -4,6 +4,7 @@ import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 interface NavLinkProps {
   href: string;
@@ -22,6 +23,15 @@ export default function Navbar() {
     session?.user?.image ||
     "https://kotonohaworks.com/free-icons/wp-content/uploads/kkrn_icon_user_1.png";
 
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(true);
+  };
+  const toggleMobileMenuClose = () => {
+    setIsMobileMenuOpen(false);
+  };
+
   return (
     <nav className="bg-white shadow-lg sticky top-0 z-50 font-sans">
       <div className="container mx-auto px-4">
@@ -31,26 +41,121 @@ export default function Navbar() {
               ImageGallery
             </p>
           </Link>
-          <div className="flex items-center space-x-4">
+          <div className="hidden sm:flex items-center space-x-4">
             <Link href={"/"}>
-              <p className={`text-gray-600 hover:text-gray-800 px-4 py-1 ${pagePath == '/' ? "border-b-4 border-custom-pink" : ""}`}>
+              <p
+                className={`text-gray-600 hover:text-gray-800 ${getNavLinkStyles(
+                  "/",
+                  pagePath
+                )}`}
+              >
                 ホーム
               </p>
             </Link>
             <Link href={"/upload"}>
-              <p className={`text-gray-600 hover:text-gray-800 px-4 py-1 ${pagePath == '/upload' ? "border-b-4 border-custom-pink" : ""}`}>
+              <p
+                className={`text-gray-600 hover:text-gray-800 ${getNavLinkStyles(
+                  "/upload",
+                  pagePath
+                )}`}
+              >
                 投稿する
               </p>
             </Link>
             <Link href={"/settings"}>
-            <p className={`text-gray-600 hover:text-gray-800 px-4 py-1 ${pagePath == '/settings' ? "border-b-4 border-custom-pink" : ""}`}>
+              <p
+                className={`text-gray-600 hover:text-gray-800 ${getNavLinkStyles(
+                  "/settings",
+                  pagePath
+                )}`}
+              >
                 設定
               </p>
             </Link>
             {session && <UserLink href="/dashboard" src={userImage} />}
             {!session && <UserLink href="/dashboard" src={userImage} />}
           </div>
+          <div className="sm:hidden">
+            <button onClick={toggleMobileMenu}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
+        {isMobileMenuOpen && (
+          <div>
+            <div
+              className="sm:hidden w-2/3 h-screen fixed transition-transform transform translate-x-0 z-20"
+              onClick={toggleMobileMenuClose}
+            ></div>
+            <div className="sm:hidden w-2/3 h-screen fixed top-0 right-0 bg-white transition-transform transform translate-x-0 z-50">
+              <div className="sm:hidden flex items-center justify-end px-4 py-5">
+                <button onClick={toggleMobileMenu}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div className="flex flex-col items-center space-y-4 py-4">
+                <Link href={"/"}>
+                  <p
+                    className={`text-gray-600 hover:text-gray-800 ${getNavLinkStyles(
+                      "/",
+                      pagePath
+                    )}`}
+                  >
+                    ホーム
+                  </p>
+                </Link>
+                <Link href={"/upload"}>
+                  <p
+                    className={`text-gray-600 hover:text-gray-800 ${getNavLinkStyles(
+                      "/upload",
+                      pagePath
+                    )}`}
+                  >
+                    投稿する
+                  </p>
+                </Link>
+                <Link href={"/settings"}>
+                  <p
+                    className={`text-gray-600 hover:text-gray-800 ${getNavLinkStyles(
+                      "/settings",
+                      pagePath
+                    )}`}
+                  >
+                    設定
+                  </p>
+                </Link>
+                {session && <UserLink href="/dashboard" src={userImage} />}
+                {!session && <UserLink href="/dashboard" src={userImage} />}
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   );
@@ -65,9 +170,15 @@ function UserLink({ href, src }: UserLinkProps) {
           alt="ユーザーアイコン"
           width={40}
           height={40}
-          className={`rounded-full transition duration-300 transform hover:scale-110`}
+          className="rounded-full transition duration-300 transform hover:scale-110"
         />
       </p>
     </Link>
   );
+}
+
+function getNavLinkStyles(linkPath: string, currentPath: string): string {
+  return linkPath === currentPath
+    ? "text-gray-800 border-b-4 border-custom-pink px-4 py-1"
+    : "px-4 py-1";
 }
