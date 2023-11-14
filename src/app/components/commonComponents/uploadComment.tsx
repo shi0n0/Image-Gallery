2e47d87846298dd188ce11cbe37b08a725fd3096
shi0n0 <shiono.yui1080@gmail.com>
2023-11-14 18:02:06 +0900

    入力した行数に合わせてtextareaを拡張

--- src/app/components/commonComponents/uploadComment.tsx
+++ src/app/components/commonComponents/uploadComment.tsx
@@ -9,7 +9,7 @@ interface ImagePath {
 const UploadComment = ({ pagePath }: ImagePath) => {
   const { data: session } = useSession();
   const userId = session?.user?.id;
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
 
   const commentData = {
     text: text,
@@ -19,15 +19,19 @@ const UploadComment = ({ pagePath }: ImagePath) => {
 
   const handleTextChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setText(event.target.value);
+
+    const lines = event.target.value.split('\n').length;
+    const rows = Math.max(lines, Math.ceil(event.target.scrollWidth / event.target.clientWidth));
+    event.target.rows = rows < 5 ? rows : 5;
   };
 
   const handleCommentSubmit = async () => {
-    const { error } = await supabase.from('Comment').insert(commentData);
+    const { error } = await supabase.from("Comment").insert(commentData);
 
     if (error) {
-      console.error('コメントの投稿に失敗しました:', error.message);
+      console.error("コメントの投稿に失敗しました:", error.message);
     } else {
-      console.log('コメントが投稿されました');
+      console.log("コメントが投稿されました");
     }
   };
 
@@ -35,10 +39,11 @@ const UploadComment = ({ pagePath }: ImagePath) => {
     <div className="p-5">
       <p className="text-xl font-bold mb-4">コメント</p>
       <textarea
-        className="w-full h-32 border border-gray-300 rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300"
+        className="w-full px-2 pb-2 pt-2 border border-gray-300 rounded-md resize-none focus:outline-none focus:ring focus:border-blue-300"
         placeholder="コメントを入力してください"
         value={text}
         onChange={handleTextChange}
+        rows={1}
       ></textarea>
       <button
         className="mt-2 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
