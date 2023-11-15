228955662bffe4bb117223bffaef1ab05b801bb4
shi0n0 <shiono.yui1080@gmail.com>
2023-11-15 15:46:35 +0900

    CSSとバグの修正

--- src/app/components/commonComponents/uploadComment.tsx
+++ src/app/components/commonComponents/uploadComment.tsx
@@ -1,12 +1,14 @@
 import supabase from "@/app/utils/supabase";
 import { useSession } from "next-auth/react";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 interface ImagePath {
   pagePath: string;
 }
 
 const UploadComment = ({ pagePath }: ImagePath) => {
+  const router = useRouter();
   const { data: session } = useSession();
   const userId = session?.user?.id;
   const [text, setText] = useState("");
@@ -20,9 +22,14 @@ const UploadComment = ({ pagePath }: ImagePath) => {
   const handleTextChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setText(event.target.value);
 
-    const lines = event.target.value.split('\n').length;
-    const rows = Math.max(lines, Math.ceil(event.target.scrollWidth / event.target.clientWidth));
-    event.target.rows = rows < 5 ? rows : 5;
+    let textarea = document.getElementById("textarea") as HTMLTextAreaElement;
+    let clientHeight = textarea.clientHeight;
+
+    textarea.addEventListener("input", () => {
+      textarea.style.height = clientHeight + "auto";
+      let scrollHeight = textarea.scrollHeight;
+      textarea.style.height = scrollHeight + "px";
+    });
   };
 
   const handleCommentSubmit = async () => {
@@ -32,6 +39,7 @@ const UploadComment = ({ pagePath }: ImagePath) => {
       console.error("コメントの投稿に失敗しました:", error.message);
     } else {
       console.log("コメントが投稿されました");
+      router.refresh();
     }
   };
 
@@ -39,15 +47,19 @@ const UploadComment = ({ pagePath }: ImagePath) => {
     <div className="p-5">
       <p className="text-xl font-bold mb-4">コメント</p>
       <textarea
-        className="w-full px-2 pb-2 pt-2 border border-gray-300 rounded-md resize-none focus:outline-none focus:ring focus:border-blue-300"
+        className="w-full px-2 pb-2 pt-2 border border-gray-300 rounded-md resize-none overflow-hidden focus:outline-none focus:ring focus:border-blue-300"
         placeholder="コメントを入力してください"
         value={text}
         onChange={handleTextChange}
         rows={1}
+        id="textarea"
       ></textarea>
       <button
         className="mt-2 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
-        onClick={handleCommentSubmit}
+        onClick={() => {
+          handleCommentSubmit();
+          router.push(pagePath);
+        }}
       >
         送信
       </button>
