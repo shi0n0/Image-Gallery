d593299d3fd74d9d3ac509079c0b2a89df41b196
shi0n0 <shiono.yui1080@gmail.com>
2023-11-18 17:27:54 +0900

    クライアントからサーバーコンポーネントに変更し、ServerActionsで実装

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,117 +1,93 @@
-"use client";
-
-import { ChangeEvent, useEffect, useState } from "react";
 import supabase from "../utils/supabase";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { nextAuthOptions } from "../api/auth/[...nextauth]/options";
 import imageSize from "image-size";
+import { redirect } from "next/navigation";
 
-export default function Upload() {
-  const { data: session } = useSession();
-  const router = useRouter();
+export default async function UploadForm() {
+  
+const UploadFile = async (formData: FormData) => {
+  "use server"
+  const file = formData.get("file") as File;
+  const title = formData.get("title");
+  const description = formData.get("description");
+  const bucket = "Images";
+  const imageTable = "Image";
+  const session = await getServerSession(nextAuthOptions);
+  const userId = session?.user?.id;
 
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+  // 選択された画像ファイルをバッファーに変換
+  const fileBuffer = await file.arrayBuffer();
+  const buffer = Buffer.from(new Uint8Array(fileBuffer));
+  const dimensions = imageSize(buffer);
+  const width = dimensions.width;
+  const height = dimensions.height;
 
-  useEffect(() => {
-    // クライアントサイドでのみ実行する初期化コードをここに配置
-    setSelectedImage(new File([], ""));
-  }, []);
-  const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files && event.target.files.length > 0) {
-      setSelectedImage(event.target.files[0]);
-    }
-  };
+  const { error:storageError } = await supabase.storage
+    .from(bucket)
+    .upload(file.name, file);
 
-  const handleSave = async () => {
-    if (!selectedImage) {
-      console.error("画像が選択されていません。");
-      return;
-    }
-    
-    // 選択された画像ファイルをバッファーに変換
-    const fileBuffer = await selectedImage.arrayBuffer();
-    const buffer = Buffer.from(new Uint8Array(fileBuffer));
+  if (storageError) {
+    console.error("ストレージエラー:",storageError);
+  } else {
+    console.log("ファイルをストレージにアップロードしました");
+  }
 
-    const dimensions = imageSize(buffer);
+  // 画像のURLを取得
+  const { data: imageData } = await supabase.storage
+    .from(bucket)
+    .getPublicUrl(file.name);
+  const imageUrl = imageData.publicUrl;
 
-    const width = dimensions.width;
-    const height = dimensions.height;
+  // 画像のURL、タイトル、および概要をDBに保存
+  const { error: databaseError } = await supabase.from(imageTable).insert([
+    {
+      url: imageUrl,
+      userId: userId,
+      title: title,
+      description: description,
+      width: width,
+      height: height,
+    },
+  ]);
 
-    const filePath = `${selectedImage.name}`;
-
-    // 画像をストレージにアップロード
-    const { error: storageError } = await supabase.storage
-      .from("Images")
-      .upload(filePath, selectedImage);
-
-    if (storageError) {
-      console.error("ストレージエラー:", storageError.message);
-      return;
-    }
-
-    // 画像のURLを取得
-    const { data } = await supabase.storage
-      .from("Images")
-      .getPublicUrl(filePath);
-    const imageUrl = data.publicUrl;
-
-    const userId = session?.user?.id;
-
-    // 画像のURL、タイトル、および概要をDBに保存
-    const { error: databaseError } = await supabase.from("Image").insert([
-      {
-        url: imageUrl,
-        userId: userId,
-        title: title,
-        description: description,
-        width: width,
-        height: height,
-      },
-    ]);
-
-    if (databaseError) {
-      console.error("データベースエラー:", databaseError.message);
-    } else {
-      console.log("データベースにデータを挿入しました。");
-      router.push("/");
-    }
-  };
+  if (databaseError) {
+    console.error("データベースエラー:", databaseError.message);
+  } else {
+    console.log("データベースにデータを挿入しました。");
+    redirect("/dashboard")
+  }
+};
 
   return (
-    <div className="max-w-lg mx-auto mt-8 p-4">
+    <form action={UploadFile} className="max-w-lg mx-auto mt-8 p-4">
       <h1 className="text-2xl font-semibold mb-4">画像をアップロード</h1>
       <div className="mb-4">
         <input
           type="file"
+          name="file"
           accept="image/*"
           className="border p-2"
-          id="file_input"
-          onChange={handleImageChange}
         />
       </div>
       <div className="mb-4">
         <input
           type="text"
+          name="title"
           placeholder="タイトル"
           className="border p-2 w-full"
-          onChange={(e) => setTitle(e.target.value)}
         />
       </div>
       <div className="mb-4">
         <textarea
+          name="description"
           placeholder="概要"
           className="border p-2 w-full h-24"
-          onChange={(e) => setDescription(e.target.value)}
         />
       </div>
-      <button
-        onClick={handleSave}
-        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-      >
-        送信
+      <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+        アップロード
       </button>
-    </div>
+    </form>
   );
-}
+};
