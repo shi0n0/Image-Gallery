375d37b51534fcfcc49dd2a602ef04d1fa19c8f2
shi0n0 <shiono.yui1080@gmail.com>
2023-11-18 17:30:39 +0900

    getServerSessionを動作させるためoptions.tsを追加

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,30 @@
+import { NextAuthOptions, Session } from 'next-auth';
+import GithubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const nextAuthOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID ?? '',
+      clientSecret: process.env.GITHUB_CLIENT_SECRET ?? '',
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+    }),
+  ],
+  callbacks: {
+    session: async ({ session, user }) => {
+      if (session?.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  }, 
+};
