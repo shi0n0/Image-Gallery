606ff89720b18108f3517981c3888a7eb246ec1f
shi0n0 <shiono.yui1080@gmail.com>
2023-11-27 17:57:38 +0900

    遷移ボタンのレイアウトとデザインを追加

--- src/app/components/commonComponents/leftbar.tsx
+++ src/app/components/commonComponents/leftbar.tsx
@@ -1,6 +1,8 @@
 "use client";
-// Sidebar.tsx
+
 import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Sidebar: React.FC = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -42,13 +44,45 @@ const Sidebar: React.FC = () => {
       )}
 
       <div
-        className={`fixed left-0 top-0 h-full bg-white w-52 z-50 transition-transform transform duration-300 text-center ${
+        className={`fixed left-0 top-0 h-full bg-white w-52 z-50 transition-transform transform duration-300 py-4 ${
           isSidebarOpen ? "translate-x-0" : "-translate-x-60"
         }`}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
       >
-        <p>要素を追加</p>
+        <p className="text-sm font-semibold text-gray-500 px-4">ホーム</p>
+        <Link href="/" className="text-md text-gray-500 flex px-4 py-2 hover:bg-gray-100">
+          <Image
+            src={"/file-circle-plus-solid.svg"}
+            alt="最近の投稿"
+            width={20}
+            height={20}
+            className="text-gray-400 m-1"
+           />
+          <p>最近のイラスト</p>
+        </Link>
+
+        <Link href="/" className="text-md text-gray-500 flex px-4 py-2 hover:bg-gray-100">
+          <Image
+            src={"/file-circle-plus-solid.svg"}
+            alt="最近の投稿"
+            width={20}
+            height={20}
+            className="text-gray-400 m-1"
+           />
+          <p>穴埋め</p>
+        </Link>
+
+        <Link href="/" className="text-md text-gray-500 flex px-4 py-2 hover:bg-gray-100">
+          <Image
+            src={"/file-circle-plus-solid.svg"}
+            alt="最近の投稿"
+            width={20}
+            height={20}
+            className="text-gray-400 m-1"
+           />
+          <p>穴埋め</p>
+        </Link>
       </div>
     </div>
   );
