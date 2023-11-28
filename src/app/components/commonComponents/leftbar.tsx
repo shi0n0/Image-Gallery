42cc118a57fbe4070aeb4338acc2d8a23e72e26f
shi0n0 <shiono.yui1080@gmail.com>
2023-11-28 15:51:00 +0900

    コンテンツを追加し、動作を改善

--- src/app/components/commonComponents/leftbar.tsx
+++ src/app/components/commonComponents/leftbar.tsx
@@ -17,7 +17,7 @@ const Sidebar: React.FC = () => {
 
   useEffect(() => {
     const handleMouseMove = (event: MouseEvent) => {
-      const shouldOpenSidebar = event.clientX <= 40;
+      const shouldOpenSidebar = event.clientX <= 20;
 
       const shouldCloseSidebar = event.clientX > 1000;
 
@@ -50,40 +50,91 @@ const Sidebar: React.FC = () => {
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
       >
-        <p className="text-sm font-semibold text-gray-500 px-4">ホーム</p>
-        <Link href="/" className="text-md text-gray-500 flex px-4 py-2 hover:bg-gray-100">
+        {/* サイドバーの中身 */}
+        {/* ロゴ兼トップページ遷移 */}
+        <div>
+          <div className="px-4">
+            <Link href={"/"} passHref className="px-3">
+              <p className="text-2xl text-center font-bold rounded text-white bg-custom-pink px-3 py-1">
+                ImageGallery
+              </p>
+            </Link>
+          </div>
+          <p className="text-sm font-semibold text-gray-500 px-5">主要</p>
+          {/* ランキング */}
+          <Link
+            href="/ranking"
+            className="text-md text-gray-500 flex px-4 py-2 hover:bg-gray-100"
+          >
+            <Image
+              src={"/file-circle-plus-solid.svg"}
+              alt="最近の投稿"
+              width={20}
+              height={20}
+              className="text-gray-400 mx-2"
+            />
+            <p>ランキング</p>
+          </Link>
+          {/* ダッシュボード */}
+          <Link
+            href="/dashboard"
+            className="text-md text-gray-500 flex px-4 py-2 mb-4 hover:bg-gray-100"
+          >
+            <Image
+              src={"/file-circle-plus-solid.svg"}
+              alt="最近の投稿"
+              width={20}
+              height={20}
+              className="text-gray-400 mx-2"
+            />
+            <p>ダッシュボード</p>
+          </Link>
+        </div>
+        {/* ホームカテゴリ */}
+        <p className="text-sm font-semibold text-gray-500 px-5">ホーム</p>
+        <Link
+          href="/"
+          className="text-md text-gray-500 flex px-4 py-2 hover:bg-gray-100"
+        >
           <Image
             src={"/file-circle-plus-solid.svg"}
             alt="最近の投稿"
             width={20}
             height={20}
-            className="text-gray-400 m-1"
-           />
+            className="text-gray-400 mx-2"
+          />
           <p>最近のイラスト</p>
         </Link>
 
-        <Link href="/" className="text-md text-gray-500 flex px-4 py-2 hover:bg-gray-100">
+        <Link
+          href="/"
+          className="text-md text-gray-500 flex px-4 py-2 hover:bg-gray-100"
+        >
           <Image
             src={"/file-circle-plus-solid.svg"}
             alt="最近の投稿"
             width={20}
             height={20}
-            className="text-gray-400 m-1"
-           />
+            className="text-gray-400 mx-2"
+          />
           <p>穴埋め</p>
         </Link>
 
-        <Link href="/" className="text-md text-gray-500 flex px-4 py-2 hover:bg-gray-100">
+        <Link
+          href="/"
+          className="text-md text-gray-500 flex px-4 py-2 hover:bg-gray-100"
+        >
           <Image
             src={"/file-circle-plus-solid.svg"}
             alt="最近の投稿"
             width={20}
             height={20}
-            className="text-gray-400 m-1"
-           />
+            className="text-gray-400 mx-2"
+          />
           <p>穴埋め</p>
         </Link>
       </div>
+      {/* サイドバーの中身終了 */}
     </div>
   );
 };
