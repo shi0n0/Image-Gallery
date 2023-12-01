5194abac050924f22100997be7c4458cd51ef89a
shi0n0 <shiono.yui1080@gmail.com>
2023-12-01 23:55:54 +0900

    一旦カードデザインの用意

--- src/app/components/commonComponents/ranking.tsx
+++ src/app/components/commonComponents/ranking.tsx
@@ -1,8 +1,25 @@
+import PaddingContainer from "./paddingCotainer";
+
 export default function Ranking() {
 
   return(
-    <div>
-      <p>ランキングページ</p>
-    </div>
+    <PaddingContainer>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+  <img className="w-full" src="/ImageGallery.png" alt="イラストの説明" />
+  <div className="px-6 py-4">
+    <div className="font-bold text-xl mb-2">イラストのタイトル</div>
+    <p className="text-gray-700 text-base">
+      作者: イラストの作者名
+    </p>
+    <p className="text-gray-700 text-base">
+      ランキング: 1位
+    </p>
+  </div>
+  <div className="px-6 py-4">
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#カテゴリ</span>
+  </div>
+</div>
+
+    </PaddingContainer>
   )
 }
