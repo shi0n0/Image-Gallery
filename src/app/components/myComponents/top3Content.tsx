d3a24bc946a8266b46bfbe5b30a9e0140515acaa
shi0n0 <shiono.yui1080@gmail.com>
2023-12-03 23:38:30 +0900

    最高視聴数の画像を取得し表示

--- src/app/components/myComponents/top3Content.tsx
+++ src/app/components/myComponents/top3Content.tsx
@@ -1,30 +1,107 @@
+import supabase from "@/app/utils/supabase";
 import Image from "next/image";
+import { getServerSession } from "next-auth";
+import { nextAuthOptions } from "@/app/api/auth/[...nextauth]/options";
 
-export default function Top3Content() {
+interface MostViewedImage {
+  id: string;
+  url: string;
+  userId: string;
+  title: string;
+  description: string;
+  width: number;
+  height: number;
+  postedAt: string;
+  viewCount: number;
+}
+
+export default async function Top3Content() {
+  const session = await getServerSession(nextAuthOptions);
+  const userId = session?.user?.id;
+  let mostViewedImage: MostViewedImage | null = {
+    id: "",
+    url: "",
+    userId: "",
+    title: "",
+    description: "",
+    width: 0,
+    height: 0,
+    postedAt: "",
+    viewCount: 0,
+  };
+
+  const getMostViewedImage = async () => {
+    const { data, error } = await supabase
+      .from("Image")
+      .select("*")
+      .eq("userId", userId)
+      .order("viewCount", { ascending: false })
+      .limit(1);
+
+    if (error) {
+      console.error("getMostViewdImageでエラーが発生しました:", error.message);
+    }
+    if (data && data.length > 0) {
+      mostViewedImage = data[0];
+      console.log("No1閲覧数の画像を取得しました");
+    } else {
+      console.log("No1閲覧数の画像を取得できませんでした");
+    }
+  };
+
+  await getMostViewedImage(); // 関数の非同期処理が完了するまで待機
+
   return (
     <div className="bg-white rounded-xl w-2/3">
       <div className="bg-gray-100 rounded-t-lg py-3 px-5 font-semibold text-xl">
         トップ3
       </div>
       <div className="flex w-full justify-center flex-col sm:flex-row">
-        <div className="py-3 px-5 font-semibold w-1/3">
-          👀視聴数
-          <div className="relative w-full pb-[100%]">
-            <Image className="rounded-lg" src="/headercat.jpeg" alt="視聴数No1コンテンツ" objectFit="cover" fill></Image>
-          </div>
-        </div>
-        <div className="py-3 px-5 font-semibold w-1/3">
-          ❤️いいね
-          <div className="relative w-full pb-[100%]">
-            <Image className="rounded-lg" src="/headercat.jpeg" alt="いいね数No1コンテンツ" objectFit="cover" fill></Image>
-          </div>
-        </div>
-        <div className="py-3 px-5 font-semibold w-1/3">
-          ⭐︎お気に入り
-          <div className="relative w-full pb-[100%]">
-            <Image className="rounded-lg" src="/headercat.jpeg" alt="お気に入り数No1コンテンツ" objectFit="cover" fill></Image>
-          </div>
-        </div>
+        {mostViewedImage ? (
+          <>
+            <div className="py-3 px-5 font-semibold w-1/3">
+              👀視聴数
+              <div className="relative w-full pb-[100%]">
+                <Image
+                  className="rounded-lg aspect-square"
+                  src={mostViewedImage.url}
+                  alt="視聴数No1コンテンツ"
+                  objectFit="cover"
+                  fill
+                />
+                <p>Title: {mostViewedImage.title}</p>
+              </div>
+            </div>
+            <div className="py-3 px-5 font-semibold w-1/3">
+              📝コメント数
+              <div className="relative w-full pb-[100%]">
+                <Image
+                  className="rounded-lg aspect-square"
+                  src={"/ImageGallery.png"}
+                  alt="コメント数No1コンテンツ"
+                  objectFit="cover"
+                  fill
+                />
+                <p>Title: 仮タイトル</p>
+              </div>
+            </div>
+            <div className="py-3 px-5 font-semibold w-1/3">
+              ❤️いいね数
+              <div className="relative w-full pb-[100%]">
+                <Image
+                  className="rounded-lg aspect-square"
+                  src={"/ImageGallery.png"}
+                  alt="いいね数No1コンテンツ"
+                  objectFit="cover"
+                  fill
+                />
+                <p>Title: 仮タイトル</p>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div>No images found for the user</div>
+        )}
       </div>
     </div>
   );
