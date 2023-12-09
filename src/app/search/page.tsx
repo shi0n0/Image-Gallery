0c6fad40439a19a1973a9ece6dc6ecdd965d621c
shi0n0 <shiono.yui1080@gmail.com>
2023-12-09 12:10:27 +0900

    画像情報を表示しでカードを追加

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -6,42 +6,99 @@ import Image from "next/image";
 import SearchBar from "../components/commonComponents/seachBar";
 import PaddingContainer from "../components/commonComponents/paddingCotainer";
 import GridContainer from "../components/commonComponents/gridContainer";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
 
-export default async function SearchResult() {
+export default function SearchResult() {
   const searchParams = useSearchParams();
   const search = searchParams.get("keyword");
+  const [imageData, setImageData] = useState<
+    { id: string; url: string; title: string; userId: string }[]
+  >([]);
+  const [userData, setUserData] = useState<
+    { id: string; image: string; name: string }[]
+  >([]);
 
-  const { data, error } = await supabase
-    .from("Image")
-    .select("*")
-    .filter("title", "ilike", `%${search}%`);
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data: fetchedImageData, error: imageError } = await supabase
+        .from("Image")
+        .select("*")
+        .filter("title", "ilike", `%${search}%`);
+
+      if (fetchedImageData) {
+        setImageData(fetchedImageData);
+
+        const userIds = fetchedImageData.map((item) => item.userId);
+        const { data: fetchedUserData, error: userError } = await supabase
+          .from("User")
+          .select("*")
+          .in("id", userIds);
+
+        if (fetchedUserData) {
+          setUserData(fetchedUserData);
+        }
+      }
+    };
+
+    fetchData();
+  }, [search]);
 
   return (
     <PaddingContainer>
       <SearchBar />
       <GridContainer>
-        {data &&
-          data.map((item) => (
-            <div key={item.id} className="bg-white rounded-sm shadow-md my-4">
-              <div className="relative aspect-square aspect-h-9">
-                <Image
-                  src={item.url}
-                  alt="検索結果"
-                  objectFit="cover"
-                  className="rounded-t-sm"
-                  fill
-                />
+        {imageData &&
+          imageData.map((item) => {
+            const user = userData.find((user) => user.id === item.userId);
+
+            return (
+              <div key={item.id} className="bg-white rounded-sm shadow-md my-4 mx-1 hover:">
+                <div className="relative aspect-square aspect-h-9">
+                  <Link href={`illustrations/${item.id}`}>
+                  <Image
+                    src={item.url}
+                    alt="検索結果"
+                    className="rounded-t-sm object-cover hover:opacity-90"
+                    fill
+                  />
+                  </Link>
+                </div>
+                <div className="p-3">
+                  <h2 className="text-sm font-bold truncate mb-1">
+                    {item.title}
+                  </h2>
+                  <div className="flex justify-between">
+                    {user && (
+                      <Link href={`/userprofile/${user.id}`}>
+                        <div className="flex items-center">
+                          <Image
+                            src={
+                              user.image ||
+                              "https://kotonohaworks.com/free-icons/wp-content/uploads/kkrn_icon_user_1.png"
+                            }
+                            alt="ユーザーアイコン"
+                            objectFit="cover"
+                            className="w-6 h-6 rounded-full"
+                            width={20}
+                            height={20}
+                          />
+                          <p className="text-gray-600 text-sm ml-1 hover:text-black truncate">
+                            {user.name || "Unknown"}
+                          </p>
+                        </div>
+                      </Link>
+                    )}
+                    <button>
+                      <FontAwesomeIcon icon={faHeart} size="xl" />
+                    </button>
+                  </div>
+                </div>
               </div>
-              <div className="py-1 px-2">
-                <h2 className="text-sm font-bold truncate">
-                  {item.title}
-                </h2>
-                <p className="text-sm text-gray-600 mb-2 truncate">
-                  {item.description}
-                </p>
-              </div>
-            </div>
-          ))}
+            );
+          })}
       </GridContainer>
     </PaddingContainer>
   );
