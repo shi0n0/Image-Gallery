8491db8b7554cbdb6081f3e616ca1e4256e8b9e4
shi0n0 <shiono.yui1080@gmail.com>
2023-12-12 19:40:37 +0900

    閲覧数の降順で情報を取得し表示

--- src/app/components/commonComponents/ranking.tsx
+++ src/app/components/commonComponents/ranking.tsx
@@ -1,25 +1,34 @@
+import supabase from "@/app/utils/supabase";
 import PaddingContainer from "./paddingCotainer";
 
-export default function Ranking() {
+export default async function Ranking() {
+  const { data, error } = await supabase
+    .from("Image")
+    .select("id, url, title, description, userId, viewCount")
+    .order("viewCount", { ascending: false });
 
-  return(
-    <PaddingContainer>
-      <div className="max-w-sm rounded overflow-hidden shadow-lg">
-  <img className="w-full" src="/ImageGallery.png" alt="イラストの説明" />
-  <div className="px-6 py-4">
-    <div className="font-bold text-xl mb-2">イラストのタイトル</div>
-    <p className="text-gray-700 text-base">
-      作者: イラストの作者名
-    </p>
-    <p className="text-gray-700 text-base">
-      ランキング: 1位
-    </p>
-  </div>
-  <div className="px-6 py-4">
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#カテゴリ</span>
-  </div>
-</div>
-
-    </PaddingContainer>
-  )
+  if (error) {
+    console.error("ランキング情報を取得中にエラーが発生:", error.message);
+  } else {
+    // ランキング情報をマップして表示
+    return (
+      <PaddingContainer>
+        {data.map((imageData, index) => (
+          <div key={imageData.id} className="max-w-sm rounded overflow-hidden shadow-lg">
+            <img className="w-full" src={imageData.url} alt={imageData.description} />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">{imageData.title}</div>
+              <p className="text-gray-700 text-base">作者: {imageData.userId}</p>
+              <p className="text-gray-700 text-base">ランキング: {index + 1}位</p>
+            </div>
+            <div className="px-6 py-4">
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                #カテゴリ
+              </span>
+            </div>
+          </div>
+        ))}
+      </PaddingContainer>
+    );
+  }
 }
