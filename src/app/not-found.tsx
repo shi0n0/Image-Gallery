aca4efc84e95edf5819d3f7a9f60cbcbd7406fc7
shi0n0 <shiono.yui1080@gmail.com>
2023-12-28 23:52:47 +0900

    not-foundを改良

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,19 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function NotFound() {
-    return(
-        <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-4xl font-bold mb-4">ページが見つかりません</h1>
-        <p className="text-gray-600 mb-8">申し訳ありませんが、お探しのページは存在しないようです。</p>
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-md text-center">
+        <h1 className="text-4xl font-bold mb-4 text-gray-800">
+          ページが見つかりません
+        </h1>
+        <p className="text-gray-600 mb-8">
+          申し訳ありませんが、お探しのページは存在しないようです。
+        </p>
         <Link href="/">
-          <p className="mt-8 text-blue-500 hover:underline">ホームに戻る</p>
+          <p className="text-blue-500 hover:underline">ホームに戻る</p>
         </Link>
       </div>
-    )
+    </div>
+  );
 }
