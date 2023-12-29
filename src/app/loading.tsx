3621e80e9e9f78d9197008e8efde454e2bac278f
shi0n0 <shiono.yui1080@gmail.com>
2023-12-29 23:48:21 +0900

    高さがscreenになっていたので削除

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return(
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center">
       <div className="w-16 h-16 mb-4">
         <div className="w-full h-full border-t-4 border-blue-500 rounded-full animate-spin"></div>
       </div>
