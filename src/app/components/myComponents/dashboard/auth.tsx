77064eb53f4297a76c3c1cfa6c1a847f7cdb0739
shi0n0 <shiono.yui1080@gmail.com>
2024-01-02 23:59:47 +0900

    デフォルトアイコンをfontawesomeにした

--- src/app/components/myComponents/dashboard/auth.tsx
+++ src/app/components/myComponents/dashboard/auth.tsx
@@ -1,6 +1,8 @@
 "use client";
 import Image from "next/image";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Auth() {
   const { data: session } = useSession();
@@ -8,35 +10,42 @@ export default function Auth() {
   return (
     <div className="z-20">
       <div className="flex items-center flex-col text-center p-3">
-        <Image
-          src={
-            session?.user?.image ||
-            "https://kotonohaworks.com/free-icons/wp-content/uploads/kkrn_icon_user_1.png"
-          }
-          alt="ユーザーアイコン"
-          width={100}
-          height={100}
-          className="rounded-full"
-        />
-        <p className="text-5xl font-medium">{session?.user?.name || "名無し"}</p>
-      {session && 
-      <button
-      onClick={() => signOut()}
-      className="text-gray-600 hover:text-gray-800 px-4 py-2"
-      >
-        <p>ログアウト</p>
-      </button>}
-      {!session && (
-        <div>
-            <p>ログインすると「いいね」「お気に入り」などの機能が開放されます👏</p>
+        {session?.user?.image && (
+          <Image
+            src={session.user.image}
+            alt="ユーザーアイコン"
+            width={100}
+            height={100}
+            className="rounded-full"
+          />
+        )}
+        {session && (
           <button
-            onClick={() => signIn()}
+            onClick={() => signOut()}
             className="text-gray-600 hover:text-gray-800 px-4 py-2"
           >
-            <p>ログイン</p>
+            <p>ログアウト</p>
           </button>
-        </div>
-      )}
+        )}
+        {!session && (
+          <div>
+              <FontAwesomeIcon
+                icon={faUser}
+                className="text-gray-800 text-5xl bg-gray-400 p-3 rounded-full aspect-square"
+              />
+            <p className="text-5xl font-medium">名無し</p>
+
+            <p>
+              ログインすると「いいね」「お気に入り」などの機能が開放されます👏
+            </p>
+            <button
+              onClick={() => signIn()}
+              className="text-gray-600 hover:text-gray-800 px-4 py-2"
+            >
+              <p>ログイン</p>
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
