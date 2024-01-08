7ab104b60159b64c0d486f25d8f405c05b211d15
shi0n0 <shiono.yui1080@gmail.com>
2024-01-09 00:18:20 +0900

    上にスピナーが寄ってしまっていたので中心になるように修正

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return(
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center w-full h-[100vh]">
       <div className="w-16 h-16 mb-4">
         <div className="w-full h-full border-t-4 border-blue-500 rounded-full animate-spin"></div>
       </div>
