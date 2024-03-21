efc293732d50190cd6a3010b8bf41442e6141be2
shi0n0 <shiono.yui1080@gmail.com>
2024-03-21 23:59:46 +0900

    トップページに横並びで適応

--- src/app/(top)/page.tsx
+++ src/app/(top)/page.tsx
@@ -2,11 +2,16 @@
 
 import TopUserCard from "../components/commonComponents/topPage/topPageCard";
 import TopPageSlider from "../components/commonComponents/topPage/topPageSlider";
+import TopPageMiniBanner from "../components/commonComponents/topPage/topPageMiniBanner";
 
 export default function Home() {
   return (
     <div>
       <TopPageSlider />
+      <div className="flex justify-center items-center space-x-4">
+        <TopPageMiniBanner href="/ranking" title="ランキングはコチラ！" />
+        <TopPageMiniBanner href="/updates" title="更新情報はコチラ！" />
+      </div>
       <TopUserCard />
     </div>
   );
