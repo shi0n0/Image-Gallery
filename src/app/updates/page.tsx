bb11d487ea42e5c0fa2d8aeb02e3fe3fb7357ef6
shi0n0 <shiono.yui1080@gmail.com>
2024-04-02 23:58:58 +0900

    選択をリセットするボタンを追加

--- src/app/updates/page.tsx
+++ src/app/updates/page.tsx
@@ -8,7 +8,8 @@ const updatesData = [
     id: "1",
     title: "ダークモード",
     category: "新機能",
-    description: "より快適な夜間視聴体験のために、ダークモードに切り替えることができるようになりました。",
+    description:
+      "より快適な夜間視聴体験のために、ダークモードに切り替えることができるようになりました。",
     date: "2024年3月20日",
     href: "darkmode",
   },
@@ -16,7 +17,8 @@ const updatesData = [
     id: "2",
     title: "検索機能の改善",
     category: "改善",
-    description: "より正確な結果とより速い応答時間を提供するために、検索が強化されました。",
+    description:
+      "より正確な結果とより速い応答時間を提供するために、検索が強化されました。",
     date: "2024年4月5日",
     href: "search-system",
   },
@@ -24,7 +26,8 @@ const updatesData = [
     id: "3",
     title: "桜の花見特集ページ開設",
     category: "開設",
-    description: "春の訪れと共に、桜の花見に最適なスポットを紹介する特集ページを開設しました。",
+    description:
+      "春の訪れと共に、桜の花見に最適なスポットを紹介する特集ページを開設しました。",
     date: "2024年3月1日",
     href: "sakura",
   },
@@ -32,7 +35,8 @@ const updatesData = [
     id: "4",
     title: "新年の挨拶カード機能追加",
     category: "新機能",
-    description: "新年のご挨拶に、オリジナルの挨拶カードを作成して送ることができる新機能をリリースしました。",
+    description:
+      "新年のご挨拶に、オリジナルの挨拶カードを作成して送ることができる新機能をリリースしました。",
     date: "2023年12月20日",
     href: "new-year",
   },
@@ -49,20 +53,28 @@ export default function UpdatesPage() {
           {updatesData.map((category, index) => (
             <button
               key={index}
-              className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-150 ease-in-out"
+              className="px-4 py-2 rounded-full font-semibold text-white bg-gradient-to-r from-blue-500 to-teal-400 hover:from-blue-600 hover:to-teal-500 shadow-lg hover:shadow-xl transform transition duration-150 ease-in-out hover:scale-105"
               // onClickに絞り込みのロジックを追加
             >
               {category.category}
             </button>
           ))}
+          <button
+            className="px-4 py-2 rounded-full font-semibold text-white bg-gradient-to-r from-gray-500 to-gray-600 hover:from-gray-600 hover:to-gray-700 shadow-lg hover:shadow-xl transform transition duration-150 ease-in-out hover:scale-105"
+          >
+            リセット
+          </button>
         </div>
         <div className="space-y-4">
           {updatesData.map((update) => (
             <Link href={`/updates/${update.href}`} key={update.id} passHref>
               <div className="block bg-white p-6 rounded-lg shadow hover:shadow-md active:scale-95 transition duration-200 ease-in-out mb-4">
                 <div className="flex flex-col">
-                  <h2 className="text-xl font-semibold text-gray-900">{update.title}</h2>
-                  <p className="text-blue-600">{update.category}</p> {/* カテゴリを表示 */}
+                  <h2 className="text-xl font-semibold text-gray-900">
+                    {update.title}
+                  </h2>
+                  <p className="text-blue-600">{update.category}</p>{" "}
+                  {/* カテゴリを表示 */}
                   <p className="mt-2 text-gray-600">{update.description}</p>
                   <p className="mt-4 text-sm text-gray-500">{update.date}</p>
                 </div>
