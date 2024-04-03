23f0b8b54e5fb55b4b39e9b65593573a8f7c7bdc
shi0n0 <shiono.yui1080@gmail.com>
2024-04-03 23:58:42 +0900

    モバイルサイズでも崩れないように修正

--- src/app/components/commonComponents/topPage/topPageMiniBanner.tsx
+++ src/app/components/commonComponents/topPage/topPageMiniBanner.tsx
@@ -9,10 +9,12 @@ interface MiniBannerProps {
 
 const TopPageMiniBanner: React.FC<MiniBannerProps> = ({ href, title }) => {
   return (
-    <Link href={href}>
-      <div className="flex items-center justify-center bg-gradient-to-r from-pink-400 to-yellow-400 text-white font-bold py-4 px-8 rounded shadow-lg mt-2 mx-2 cursor-pointer transition duration-300 ease-in-out transform hover:scale-105">
-        <span className="block text-center text-lg mr-2">{title}</span>
-        <FontAwesomeIcon icon={faArrowRight} className="h-5 w-5" />
+    <Link href={href} passHref>
+      <div className="block">
+        <div className="flex items-center justify-center bg-gradient-to-r from-pink-400 to-yellow-400 text-white font-bold py-2 px-4 sm:py-3 sm:px-6 md:py-4 md:px-8 rounded shadow-lg mt-2 mx-2 cursor-pointer transition duration-300 ease-in-out transform hover:scale-105">
+          <span className="text-center text-xs sm:text-sm md:text-lg mr-2">{title}</span>
+          <FontAwesomeIcon icon={faArrowRight} className="h-3 w-3 sm:h-4 sm:w-4 md:h-5 md:w-5" />
+        </div>
       </div>
     </Link>
   );
