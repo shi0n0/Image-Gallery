0d07be1152317daee8c27fbf7ada5dba444602b4
shi0n0 <shiono.yui1080@gmail.com>
2024-04-09 23:57:38 +0900

    Loadingのテキストを任意で変更できるようにした

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,10 +1,16 @@
-export default function Loading() {
-  return(
+import React from 'react';
+
+interface LoadingProps {
+  text?: string;
+}
+
+export default function Loading({ text = "ページを読み込んでいます..." }: LoadingProps) {
+  return (
     <div className="flex flex-col items-center justify-center w-full h-[100vh]">
       <div className="w-16 h-16 mb-4">
         <div className="w-full h-full border-t-4 border-blue-500 rounded-full animate-spin"></div>
       </div>
-      <p className="text-lg font-semibold text-blue-500">ページを読み込んでいます...</p>
+      <p className="text-lg font-semibold text-blue-500">{text}</p>
     </div>
-  )
+  );
 }
