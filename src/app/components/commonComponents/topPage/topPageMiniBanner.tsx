4a1ace0279032accb7bde94c328b99105a447a5a
shi0n0 <shiono.yui1080@gmail.com>
2024-04-11 22:07:37 +0900

    mt-10 にして縦幅を均等にした

--- src/app/components/commonComponents/topPage/topPageMiniBanner.tsx
+++ src/app/components/commonComponents/topPage/topPageMiniBanner.tsx
@@ -11,9 +11,14 @@ const TopPageMiniBanner: React.FC<MiniBannerProps> = ({ href, title }) => {
   return (
     <Link href={href} passHref>
       <div className="block">
-        <div className="flex items-center justify-center bg-gradient-to-r from-pink-400 to-yellow-400 text-white font-bold py-2 px-4 sm:py-3 sm:px-6 md:py-4 md:px-8 rounded shadow-lg mt-2 mx-2 cursor-pointer transition duration-300 ease-in-out transform hover:scale-105">
-          <span className="text-center text-xs sm:text-sm md:text-lg mr-2">{title}</span>
-          <FontAwesomeIcon icon={faArrowRight} className="h-3 w-3 sm:h-4 sm:w-4 md:h-5 md:w-5" />
+        <div className="flex items-center justify-center bg-gradient-to-r from-pink-400 to-yellow-400 text-white font-bold py-2 px-4 sm:py-3 sm:px-6 md:py-4 md:px-8 rounded shadow-lg mt-10 mx-2 cursor-pointer transition duration-300 ease-in-out transform hover:scale-105">
+          <span className="text-center text-xs sm:text-sm md:text-lg mr-2">
+            {title}
+          </span>
+          <FontAwesomeIcon
+            icon={faArrowRight}
+            className="h-3 w-3 sm:h-4 sm:w-4 md:h-5 md:w-5"
+          />
         </div>
       </div>
     </Link>
