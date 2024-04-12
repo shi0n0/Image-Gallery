dc738f0ce08a7d0d3b49a8eb9b6d8ed0e15fb8fa
shi0n0 <shiono.yui1080@gmail.com>
2024-04-12 23:19:10 +0900

    ロゴをクリックでトップに遷移

--- src/app/components/commonComponents/navigation/footer.tsx
+++ src/app/components/commonComponents/navigation/footer.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Facebook, Instagram, Twitter } from "lucide-react";
+import Link from "next/link";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -8,19 +9,19 @@ const Footer = () => {
   return (
     <footer className="bg-white text-gray-600 body-font border-t">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+        <Link href={"/"} className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
           <span className="ml-3 text-xl">ImageGallery</span>
-        </a>
+        </Link>
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
           © {currentYear} ImageGallery —
-          <a
+          <Link
             href="mailto:contact@yoursite.com"
             className="text-gray-600 ml-1"
             rel="noopener noreferrer"
             target="_blank"
           >
             contact@testsite.com
-          </a>
+          </Link>
         </p>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
           <a className="text-gray-500" href="https://twitter.com/yourtwitter">
