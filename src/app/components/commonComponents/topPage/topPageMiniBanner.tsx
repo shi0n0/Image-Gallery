61c0f1c82fe2d1ec766ff0949460b38ce7c9def9
shi0n0 <shiono.yui1080@gmail.com>
2024-04-23 00:00:00 +0900

    クリック時の効果を追加

--- src/app/components/commonComponents/topPage/topPageMiniBanner.tsx
+++ src/app/components/commonComponents/topPage/topPageMiniBanner.tsx
@@ -11,7 +11,7 @@ const TopPageMiniBanner: React.FC<MiniBannerProps> = ({ href, title }) => {
   return (
     <Link href={href} passHref>
       <div className="block">
-        <div className="flex items-center justify-center bg-gradient-to-r from-pink-400 to-yellow-400 text-white font-bold py-2 px-4 sm:py-3 sm:px-6 md:py-4 md:px-8 rounded shadow-lg mt-10 mx-2 cursor-pointer transition duration-300 ease-in-out transform hover:scale-105">
+        <div className="flex items-center justify-center bg-gradient-to-r from-pink-400 to-yellow-400 text-white font-bold py-2 px-4 sm:py-3 sm:px-6 md:py-4 md:px-8 rounded shadow-lg mt-10 mx-2 cursor-pointer transition duration-300 ease-in-out transform hover:scale-105 active:brightness-90 active:duration-100">
           <span className="text-center text-xs sm:text-sm md:text-lg mr-2">
             {title}
           </span>
