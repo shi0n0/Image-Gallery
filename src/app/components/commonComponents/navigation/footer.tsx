78f2155163608334792db195d1f5ee889168f0b8
shi0n0 <shiono.yui1080@gmail.com>
2024-05-02 23:57:53 +0900

    アイコンをホバーで文字色が濃くなるようにした

--- src/app/components/commonComponents/navigation/footer.tsx
+++ src/app/components/commonComponents/navigation/footer.tsx
@@ -9,7 +9,10 @@ const Footer = () => {
   return (
     <footer className="bg-white text-gray-600 body-font border-t">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <Link href={"/"} className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+        <Link
+          href={"/"}
+          className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900"
+        >
           <span className="ml-3 text-xl">ImageGallery</span>
         </Link>
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
@@ -24,17 +27,17 @@ const Footer = () => {
           </Link>
         </p>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-          <a className="text-gray-500" href="https://twitter.com/yourtwitter">
+          <a className="text-gray-500 hover:text-gray-600" href="https://twitter.com/yourtwitter">
             <Twitter className="w-5 h-5" />
           </a>
           <a
-            className="ml-3 text-gray-500"
+            className="ml-3 text-gray-500 hover:text-gray-600"
             href="https://instagram.com/yourinstagram"
           >
             <Instagram className="w-5 h-5" />
           </a>
           <a
-            className="ml-3 text-gray-500"
+            className="ml-3 text-gray-500 hover:text-gray-600"
             href="https://facebook.com/yourfacebook"
           >
             <Facebook className="w-5 h-5" />
